3eb422b249e8eff284ac2de8ca5a14c893a5b5c0
Cameron <cameronkdb@gmail.com>
2021-09-25 16:42:23 +0200

    Updated project files

---
+++ AccountSystem/AccountSystemDomain/src/main/java/za/ac/nwu/ac/domain/service/GeneralResponse.java
@@ -0,0 +1,48 @@
+package za.ac.nwu.ac.domain.service;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+public class GeneralResponse<T> implements Serializable{
+
+    private final boolean successfull;
+    private final transient T payload;
+
+    public GeneralResponse(boolean successfull, T payload) {
+        this.successfull = successfull;
+        this.payload = payload;
+    }
+
+    public boolean isSuccessfull() {
+        return successfull;
+    }
+
+    public T getPayload() {
+        return payload;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o)
+            return true;
+
+        if (o == null || getClass() != o.getClass())
+            return false;
+
+        GeneralResponse<?> that = (GeneralResponse<?>) o;
+        return successfull == that.successfull && Objects.equals(payload, that.payload);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(successfull, payload);
+    }
+
+    @Override
+    public String toString() {
+        return "GeneralResponse{"+
+                "successful=" + successfull +
+                ", payload=" + payload +
+                "}";
+    }
+}
