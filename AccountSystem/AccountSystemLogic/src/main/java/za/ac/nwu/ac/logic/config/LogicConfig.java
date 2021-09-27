092fe31beba1e050c7a9774fea83278e6ac8a108
Cameron <cameronkdb@gmail.com>
2021-09-27 14:26:25 +0200

    Complete rework after error occurred

--- AccountSystem/AccountSystemLogic/src/main/java/za/ac/nwu/ac/logic/config/LogicConfig.java
+++ AccountSystem/AccountSystemLogic/src/main/java/za/ac/nwu/ac/logic/config/LogicConfig.java
@@ -1,4 +1,9 @@
 package za.ac.nwu.ac.logic.config;
 
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+import za.ac.nwu.ac.translator.config.TranslatorConfig;
+
+@Import({TranslatorConfig.class})
 public class LogicConfig {
 }
