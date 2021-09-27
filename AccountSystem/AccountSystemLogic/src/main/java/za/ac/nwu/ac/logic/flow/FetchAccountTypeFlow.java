6fe90c2ace363037f7e3d67cafc3790cb2789a5a
Cameron <cameronkdb@gmail.com>
2021-09-27 12:50:24 +0200

    Additional POM work

--- AccountSystem/AccountSystemLogic/src/main/java/za/ac/nwu/ac/logic/flow/FetchAccountTypeFlow.java
+++ AccountSystem/AccountSystemLogic/src/main/java/za/ac/nwu/ac/logic/flow/FetchAccountTypeFlow.java
@@ -1,2 +1,8 @@
-package za.ac.nwu.ac.logic.flow;public interface FetchAccountTypeFlow {
+package za.ac.nwu.ac.logic.flow;
+
+import java.util.List;
+import za.ac.nwu.ac.domain.dto.AccountTypeDto;
+
+public interface FetchAccountTypeFlow {
+    List<AccountTypeDto> getAllAccountTypes();
 }
