ab61262bfef360fa2e76ef11a26c49730affca62
Cameron <cameronkdb@gmail.com>
2021-09-27 13:31:10 +0200

    Revert "Additional POM work"
    This reverts commit 6fe90c2ace363037f7e3d67cafc3790cb2789a5a.

--- AccountSystem/AccountSystemLogic/src/main/java/za/ac/nwu/ac/logic/flow/FetchAccountTypeFlow.java
+++ AccountSystem/AccountSystemLogic/src/main/java/za/ac/nwu/ac/logic/flow/FetchAccountTypeFlow.java
@@ -1,8 +1,2 @@
-package za.ac.nwu.ac.logic.flow;
-
-import java.util.List;
-import za.ac.nwu.ac.domain.dto.AccountTypeDto;
-
-public interface FetchAccountTypeFlow {
-    List<AccountTypeDto> getAllAccountTypes();
+package za.ac.nwu.ac.logic.flow;public interface FetchAccountTypeFlow {
 }
