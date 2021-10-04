90cf49569b97833249c8e528506f6467b88585a4
Cameron <cameronkdb@gmail.com>
2021-10-04 16:37:26 +0200

    Must haves sorted? (Kinda)

--- AccountSystem/AccountSystemTranslator/src/main/java/za/ac/nwu/ac/translator/impl/AccountTransactionTranslatorImpl.java
+++ AccountSystem/AccountSystemTranslator/src/main/java/za/ac/nwu/ac/translator/impl/AccountTransactionTranslatorImpl.java
@@ -1,54 +1,66 @@
 package za.ac.nwu.ac.translator.impl;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 import za.ac.nwu.ac.domain.dto.AccountTransactionDto;
 import za.ac.nwu.ac.domain.persistence.AccountTransaction;
 import za.ac.nwu.ac.repo.persistence.AccountTransactionRepository;
 import za.ac.nwu.ac.translator.AccountTransactionTranslator;
-import org.springframework.stereotype.Component;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @Component
-public class AccountTransactionTranslatorImpl  implements AccountTransactionTranslator{
-    private AccountTransactionRepository repo;
+public class AccountTransactionTranslatorImpl implements AccountTransactionTranslator {
 
-    public AccountTransactionTranslatorImpl(AccountTransactionRepository accountTransactionRepository){
-        this.repo = accountTransactionRepository;
+    private final AccountTransactionRepository accountTransactionRepository;
+
+    @Autowired
+    public AccountTransactionTranslatorImpl(AccountTransactionRepository accountTransactionRepository)
+    {
+        this.accountTransactionRepository = accountTransactionRepository;
     }
 
     @Override
-    public AccountTransaction save(AccountTransaction accountTransaction){
-        try {
-            return repo.save(accountTransaction);
-        }catch (Exception e) {
-            throw new RuntimeException("Unable to save to the DB", e);
-        }
-    }
-
-
-    @Override
-    public List<AccountTransaction> getAllAccountTransactions(){
-        List<AccountTransaction> accountTransactions;
-        try {
-            accountTransactions = new ArrayList<>(repo.findAll());
-        }catch (Exception e) {
-            throw new RuntimeException("unable to save to the DB", e);
-        }
-        return accountTransactions;
-    }
-
-    @Override
-    public AccountTransaction getAccountTransactionByPk(Long transactionId){
-        try {
-            return repo.findById(transactionId).orElse(null);
-        }catch (Exception e) {
-            throw new RuntimeException("Unable to save to the DB", e);
-        }
-    }
-
-    @Override
-    public AccountTransactionDto getAccountTypeDbEntityByMnemonic(String mnemonic) {
+    public AccountTransactionDto save(AccountTransactionDto accountTransaction) {
         return null;
     }
+
+    @Override
+    public List<AccountTransactionDto> getAllAccountTransactions()
+    {
+        List<AccountTransactionDto> accountTransactionDto = new ArrayList<>();
+        try{
+            for (AccountTransaction accountTransaction : accountTransactionRepository.findAll()){
+                accountTransactionDto.add(new AccountTransactionDto(accountTransaction));
+            }
+        } catch (Exception e) {
+            throw new RuntimeException("Unable to read from the DB", e);
+        }
+        return accountTransactionDto;
+
+    }
+
+    @Override
+    public AccountTransactionDto create(AccountTransactionDto accountTransactionDto)
+    {
+        try{
+            AccountTransaction accountTransaction = accountTransactionRepository.save(accountTransactionDto.getTransactionType());
+            return new AccountTransactionDto(accountTransaction);
+        } catch (Exception e){
+            throw new RuntimeException("Unable to save to the DB", e);
+        }
+
+    }
+
+    @Override
+    public AccountTransactionDto getAccountTransactionDtoByMemberId(Long memberId) {
+        try{
+            return accountTransactionRepository.getAccountTransactionDtoByMember(memberId);
+        }
+        catch (Exception e){
+            throw new RuntimeException("Unable to read from the DB", e);
+        }
+    }
+
 }
